7a0ca458b8c519c1002a45c34f0b1f543c5fa426
Jamil Kawsher <jamil15-9734@diu.edu.bd>
2021-11-12 02:20:47 +0600

    database connected and products app get creted

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 5000 || process.env.PORT
+const express = require('express');
+const app = express();
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l9s2s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log("Database Connected successfuylly")
+        const database = client.db("actionShark");
+        const productsCollection = database.collection("products");
+        //geting all product from database
+        app.get('/products', async (req, res) => {
+            const query = productsCollection.find({});
+            const result = await query.toArray();
+            res.send(result)
+        })
 
 
-
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello From Action Shark')
 })
+
 
 app.listen(port, () => {
     console.log(`listening at :${port}`)
