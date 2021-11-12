dd272b89ed15b1cc37a3e3b50c2ef7e4a64647dd
Jamil Kawsher <jamil15-9734@diu.edu.bd>
2021-11-12 11:38:25 +0600

    Add product make admin user info get post put method added

--- index.js
+++ index.js
@@ -15,11 +15,59 @@ async function run() {
         // console.log("Database Connected successfuylly")
         const database = client.db("actionShark");
         const productsCollection = database.collection("products");
+        const usersCollection = database.collection('users');
         //geting all product from database
         app.get('/products', async (req, res) => {
             const query = productsCollection.find({});
             const result = await query.toArray();
             res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+        app.put('/users', async (req, res) => {
+
+            const email = req.body.email;
+            const user = req.body;
+            const displayName = req.body.displayName;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    email: email,
+                    displayName: displayName,
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+
+
+
+
+        })
+
+
+        //make an admin
+        app.put('/users/admin', async (req, res) => {
+
+            const user = req.body;
+            console.log(req);
+
+            const filter = { email: user.email };
+
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+
+        // add product
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.send(result);
         })
 
 
